31a9af0b449419c7462850cd9ca9e99a48df2d47
Eduardo Vicente <eduardomcv42@gmail.com>
2022-11-13 20:02:29 +0000

    Add query client and provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Outlet, RouterProvider } from '@tanstack/react-router';
 import AppHeader from './components/AppHeader';
 import router from './routes/router';
 
+const client = new QueryClient();
+
 function App() {
   return (
-    <RouterProvider router={router}>
-      <AppHeader />
-      <Outlet />
-    </RouterProvider>
+    <QueryClientProvider client={client}>
+      <RouterProvider router={router}>
+        <AppHeader />
+        <Outlet />
+      </RouterProvider>
+    </QueryClientProvider>
   );
 }
 
