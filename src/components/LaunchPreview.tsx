04171611bb4d0508cc9fda547443cbb738ae2015
Eduardo Vicente <eduardomcv42@gmail.com>
2022-11-13 20:34:52 +0000

    Change to Launch preview.

---
+++ src/components/LaunchPreview.tsx
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid hsl(199 38% 30%);
+  border-radius: 5%;
+  aspect-ratio: 1/1;
+
+  :hover {
+    background-color: hsl(199 38% 30%);
+  }
+`;
+
+const Image = styled.img`
+  max-width: 13rem;
+  max-height: 12rem;
+`;
+
+const Text = styled.p`
+  text-align: center;
+  margin: 1rem 0 0 0;
+`;
+
+interface LaunchPreviewProps {
+  missionName: string;
+  rocketName: string;
+  image?: string;
+}
+
+export default function LaunchPreview({
+  missionName,
+  rocketName,
+  image = '',
+}: LaunchPreviewProps) {
+  return (
+    <Container>
+      {image && <Image src={image} alt={missionName} />}
+      <Text>Mission: {missionName}</Text>
+      <Text>Rocket: {rocketName}</Text>
+    </Container>
+  );
+}
