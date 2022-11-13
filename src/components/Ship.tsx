f6a300eed8a6cb499c98ba0a2b91f2a4d66cc387
Eduardo Vicente <eduardomcv42@gmail.com>
2022-11-13 17:46:40 +0000

    Add container and ship image components

--- src/components/Ship.tsx
+++ src/components/Ship.tsx
@@ -1,8 +1,24 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  color: white;
+  display: grid;
+  place-content: center;
+  border: 1px solid hsl(199 38% 30%);
+  border-radius: 5%;
+  aspect-ratio: 1/1;
+`;
+
+const ShipImage = styled.img`
+  aspect-ratio: 1/1;
+  height: 8rem;
+`;
+
 export default function Ship() {
   return (
-    <div>
-      <img src="" alt="ship" />
+    <Container>
+      <ShipImage src="" alt="ship" />
       <p>ship name</p>
-    </div>
+    </Container>
   );
 }
