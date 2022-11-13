34841083f1da8ea1264fa9d0ea8c0b2b38c5ec02
Eduardo Vicente <eduardomcv42@gmail.com>
2022-11-13 16:39:06 +0000

    Add grid and list item components

---
+++ src/components/GridList.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export default styled.ul`
+  display: grid;
+  margin: unset;
+  padding: unset;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 1rem;
+`;
---
+++ src/components/ListItem.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export default styled.li`
+  list-style: none;
+  display: grid;
+  place-items: center;
+`;
