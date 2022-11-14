579c61961e52db609e4fcf0b847bb48b727b5e8f
Eduardo Vicente <eduardomcv42@gmail.com>
2022-11-14 10:43:10 +0000

    Add button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export default styled.button`
+  padding: 0.5rem;
+  margin: 1rem 0;
+  background-color: #68bdff;
+  border: unset;
+  border-radius: 10%;
+  cursor: pointer;
+
+  :hover {
+    background-color: #84caff;
+  }
+
+  :active {
+    background-color: #3396e1;
+  }
+`;
