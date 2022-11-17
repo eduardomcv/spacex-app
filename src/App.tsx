ca610f31e5473587718fb86877f1fcef3d35cc89
Eduardo Vicente <eduardomcv42@gmail.com>
2022-11-17 23:12:25 +0000

    Add router devtools component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Outlet, RouterProvider } from '@tanstack/react-router';
+import { lazy } from 'react';
 import AppHeader from './components/AppHeader';
-import router from './routes/router';
+import { router } from './routes/router';
 
 const client = new QueryClient();
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === 'production'
+    ? () => null
+    : lazy(async () => {
+        const res = await import('@tanstack/react-router-devtools');
+        return { default: res.TanStackRouterDevtools };
+      });
 
 function App() {
   return (
@@ -12,6 +21,7 @@ function App() {
         <AppHeader />
         <Outlet />
       </RouterProvider>
+      <TanStackRouterDevtools router={router} />
     </QueryClientProvider>
   );
 }
