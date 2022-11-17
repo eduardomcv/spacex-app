7a440d76091d3fbc21a3e61eda3d145710eb9fb8
Eduardo Vicente <eduardomcv42@gmail.com>
2022-11-17 23:20:30 +0000

    Add first draft of Launch component

---
+++ src/routes/Launch.tsx
@@ -0,0 +1,10 @@
+export default function Launch() {
+  const searchParams = new URLSearchParams(window.location.search);
+  const id = searchParams.get('id');
+
+  return (
+    <main>
+      <img alt="" />
+    </main>
+  );
+}
